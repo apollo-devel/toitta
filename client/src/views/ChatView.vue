57cb2d030b0611439055d44d546f285466e42c11
apollo.devel <apollo.devel@gmail.com>
2022-04-03 16:05:31 +0900

    42 チャット機能(4)

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -6,38 +6,172 @@
     <div class="uk-text-bold">チャットのタイトル</div>
     <hr class="top-hr" />
     <div class="uk-flex-1 uk-flex uk-flex-column content">
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
-      <div>hi</div>
-      <div>hello</div>
+      <div class="message mine uk-flex">
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image"></div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="sender uk-text-muted uk-text-small">hoge</span>
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image"></div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image">
+          <img src="https://ui-avatars.com/api/?name=?" />
+        </div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+
+      <div class="message mine uk-flex">
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image"></div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="sender uk-text-muted uk-text-small">hoge</span>
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image"></div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image">
+          <img src="https://ui-avatars.com/api/?name=?" />
+        </div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+
+      <div class="message mine uk-flex">
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image"></div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="sender uk-text-muted uk-text-small">hoge</span>
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image"></div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image">
+          <img src="https://ui-avatars.com/api/?name=?" />
+        </div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+
+      <div class="message mine uk-flex">
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image"></div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="sender uk-text-muted uk-text-small">hoge</span>
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image"></div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image">
+          <img src="https://ui-avatars.com/api/?name=?" />
+        </div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image"></div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="sender uk-text-muted uk-text-small">hoge</span>
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image"></div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
+      <div class="message theirs uk-flex">
+        <div class="image">
+          <img src="https://ui-avatars.com/api/?name=?" />
+        </div>
+        <div class="container uk-flex uk-flex-column">
+          <span class="body uk-text-small">
+            hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi hi
+          </span>
+        </div>
+      </div>
     </div>
     <hr class="bottom-hr" />
     <div class="uk-flex uk-flex-row uk-flex-middle uk-margin-small-bottom">
@@ -51,6 +185,7 @@
 .content {
   overflow-y: auto;
   margin: 0;
+  padding: 5px;
 }
 
 .top-hr {
@@ -59,5 +194,41 @@
 
 .bottom-hr {
   margin-top: 0;
+}
+
+.message {
+  align-items: flex-end;
+  padding-bottom: 4px;
+}
+
+.message.mine {
+  flex-direction: row-reverse;
+}
+
+.container {
+  max-width: 55%;
+}
+
+.body {
+  background-color: #f1f0f0;
+  padding: 6px 12px;
+  border-radius: 18px;
+}
+
+.mine .body {
+  background-color: #1fa2f1;
+  color: white;
+}
+
+.image {
+  height: 24px;
+  width: 24px;
+  margin-right: 7px;
+}
+
+.image img {
+  height: 100%;
+  border-radius: 50%;
+  vertical-align: bottom;
 }
 </style>
